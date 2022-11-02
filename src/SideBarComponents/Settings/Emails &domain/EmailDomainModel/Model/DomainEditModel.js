42ef4c6785b96083537c4719fa996c08eecb19e2
Somil Kaushal <somilkaushal89@gmail.com>
2022-11-02 11:44:33 +0530

    resolving issus

--- src/SideBarComponents/Settings/Emails &domain/EmailDomainModel/Model/DomainEditModel.js
+++ src/SideBarComponents/Settings/Emails &domain/EmailDomainModel/Model/DomainEditModel.js
@@ -1,20 +1,21 @@
 import React from "react";
-import './DomainEditModel.css'
+import "./DomainEditModel.css";
 import Box from "@mui/material/Box";
 import ClearOutlinedIcon from "@mui/icons-material/ClearOutlined";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
 import Switch from "@mui/material/Switch";
-<<<<<<< Updated upstream
-import Alert from '@mui/material/Alert';
-import AlertTitle from '@mui/material/AlertTitle';
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
 import { IconButton } from "@mui/material";
-=======
 import { useState } from "react";
 import ResetSMTPmodal from "./ResetSMTPmodal";
-import { IconButton } from "@mui/material";
-
->>>>>>> Stashed changes
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import TextField from "@mui/material/TextField";
+import AddIcon from "@mui/icons-material/Add";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Chip from "@mui/material/Chip";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
 export default function DomainEditModal(props) {
   const [resetsmtpmod, setResetsmtpmod] = useState(false);
   const closeresetsmtpmodal = () => {
@@ -22,38 +23,42 @@ export default function DomainEditModal(props) {
   };
   return (
     <Drawer anchor={"right"} open={props.open} onClose={props.close}>
-<<<<<<< Updated upstream
       <Box role="presentation" className="SmtpEditModel">
         <div>
-          <IconButton style={{ Margin: '8px' }}>
-            <ClearOutlinedIcon
-              color="action"
-              onClick={props.close}
-            />
+          <IconButton style={{ Margin: "8px" }}>
+            <ClearOutlinedIcon color="action" onClick={props.close} />
           </IconButton>
         </div>
         <div className="SmtpEditModelHead">
           <div>
-            <div>
-              <Alert severity="warning">
-                <AlertTitle>DMARC has issues</AlertTitle>
-                Please check DMARC Settings.
+            <Alert severity="warning">
+              <AlertTitle>DMARC has issues</AlertTitle>
+              Please check DMARC Settings.
+            </Alert>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              gap: "15px",
+              alignItems: "center",
+              backgroundColor: "#f7f7f7 ",
+              marginTop: "20px",
+            }}
+          >
+            <div style={{ padding: "16px" }}>
+              <div className="ProfileSection">
+                <AccountCircleIcon
+                  color="warning"
+                  style={{ fontSize: "40px" }}
+                />
+                <span>somilkaushal89@gmail.com</span>
+              </div>
+              <Alert severity="success">
+                <AlertTitle>Mail Account is available for use</AlertTitle>
+                Emails may be sent from this account. Inbox will be continuously
+                checked for incoming replies.
               </Alert>
             </div>
-=======
-      <Box sx={{ width: 385 }} role="presentation">
-        <IconButton onClick={props.close} style={{ padding: "8px" }}>
-          <ClearOutlinedIcon color="action" className="closebtn" />
-        </IconButton>
-        <br />
-        <div style={{ width: "100%", padding: "0px 32px" }}>
-          <div style={{ fontSize: "1.5rem", marginBottom: "56px" }}>
-            Edit email warm up
->>>>>>> Stashed changes
-          </div>
-          <div style={{ fontSize: "0.875rem", color: "rgba(0, 0, 0, 0.6)" }}>
-            <Switch color="warning" />
-            Running
           </div>
           <div
             style={{
@@ -62,15 +67,73 @@ export default function DomainEditModal(props) {
               marginTop: "40px",
             }}
           >
+            <Button className="newtemplatebtn">Show Sending Schedule</Button>
+          </div>
+          <div className="AccountstatusText">Account status</div>
+          <div style={{ fontSize: "0.875rem", color: "rgba(0, 0, 0, 0.6)" }}>
+            <Switch color="warning" />
+            Enable account
+          </div>
+          <div className="AccountstatusText">Account Auto Rate Limiting</div>
+          <div style={{ marginTop: "10px" }}>
+            <TextField
+              id="outlined-basic"
+              label="24H sending limit"
+              variant="outlined"
+              type="Number"
+              color="warning"
+              size="small"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </div>
+          <div className="emailsperTextDiv">
+            <span className="emailsperText">
+              This mail account may send up to 99 emails per 24 hours
+            </span>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              margin: "48px 0px",
+            }}
+          >
             <Button
               className="newtemplatebtn"
-              style={{ width: "100%" }}
               onClick={() => {
                 setResetsmtpmod(true);
               }}
             >
-              Save
+              Reset SMTP credentials
             </Button>
+          </div>
+          <div className="AddFromBtN">
+            <div className="AccountstatusText" style={{ margin: "0px" }}>
+              Sent emails from
+            </div>
+            <Button className="newtemplatebtn">
+              <AddIcon />
+              Add From
+            </Button>
+          </div>
+          <div className="MoreVertIconBtn">
+            <div style={{ display: "flex" }}>
+              <div>
+                <MailOutlineIcon color="warning" />
+              </div>
+
+              <div>
+                <Chip label="Active" color="success" size="small" />
+              </div>
+            </div>
+
+            <div>
+              <IconButton>
+                <MoreVertIcon />
+              </IconButton>
+            </div>
           </div>
         </div>
       </Box>
